9eed5fb73a5bd5f0b4a55337c41fe2c03ad210ab
Kvark900 <kvant800@gmail.com>
2020-01-03 17:32:55 +0100

    question finder, update question feat

--- routes/index.js
+++ routes/index.js
@@ -37,20 +37,80 @@ router.delete("/survey/:id", (async (req, res) => {
 }));
 
 
-router.get('/question', async (req, res, next) => {
-    let questions = await dao.getQuestions();
+router.get('/addQuestion', async (req, res, next) => {
     let lectures = await dao.getLecturesWithSubjectNames();
     let types = await dao.getQuestionTypes();
     let categories = await dao.getQuestionCategories();
-    res.render('question',
+    res.render('addQuestion',
         {
             title: 'Question',
-            moment:  moment,
-            questions: questions.rows,
+            moment: moment,
             lectures: lectures.rows,
             types: types.rows,
             categories: categories.rows
         });
+});
+
+
+router.get('/updateQuestion/:id', async (req, res, next) => {
+    let lectures = await dao.getLecturesWithSubjectNames();
+    let types = await dao.getQuestionTypes();
+    let categories = await dao.getQuestionCategories();
+    let current = await dao.getQuestion(req.params.id);
+    res.render('addQuestion',
+        {
+            title: 'Question',
+            moment: moment,
+            lectures: lectures.rows,
+            types: types.rows,
+            categories: categories.rows,
+            current: current.rows[0]
+        });
+});
+
+router.get('/question', async (req, res, next) => {
+    let subjects = await dao.getSubjectsByProfessorId(2); // TODO: get currently logged in professor's id
+    let lectures = await dao.getLecturesWithSubjectNames();
+    res.render('questionFinder',
+        {
+            title: 'Question',
+            moment: moment,
+            axios: axios,
+            subjects: subjects.rows,
+            lectures: lectures.rows,
+        });
+});
+
+
+router.delete("/api/question/:id", (async (req, res) => {
+    try {
+        console.info("Deleting question with id: " + req.params.id);
+        await dao.deleteQuestion(req.params.id);
+        res.sendStatus(200);
+    } catch (e) {
+        res.status(400).send(e.message)
+    }
+}));
+
+router.put("/api/question/:id", (async (req, res) => {
+    try {
+        console.info("Updating question with id: " + req.params.id);
+        await dao.updateQuestion(req.body);
+        res.sendStatus(200);
+    } catch (e) {
+        res.status(400).send(e.message)
+    }
+}));
+
+
+
+router.get('/api/question/:lectureId', async (req, res, next) => {
+    try {
+        let questions = await dao.getQuestionsByLectureId(req.params.lectureId);
+        res.status(200).send(questions.rows);
+    } catch (e) {
+        res.status(400).send(e.message)
+    }
 });
 
 router.post("/question", async (req, res) => {
--- services/dao.js
+++ services/dao.js
@@ -73,10 +73,10 @@ async function getSurveysWithSubjectNames() {
 async function getLecturesWithSubjectNames() {
     try {
         let queryResult = await dbConfig.pool.query(
-                         "SELECT l.*, s.name subject_name " +
-                         "FROM survey_copy.lecture l " +
-                         "JOIN survey_copy.subject s " +
-                         "ON l.subject_id = s.id");
+            "SELECT l.*, s.name subject_name " +
+            "FROM survey_copy.lecture l " +
+            "JOIN survey_copy.subject s " +
+            "ON l.subject_id = s.id");
         console.info(new Date() + ": Getting lectures with subject names success");
         return queryResult;
     } catch (e) {
@@ -94,10 +94,59 @@ async function deleteSurvey(id) {
     }
 }
 
+async function deleteQuestion(id) {
+    try {
+        await dbConfig.pool.query("DELETE FROM survey_copy.question WHERE id = $1", [id]);
+    } catch (e) {
+        console.error(e);
+        throw e;
+    }
+}
+
+async function updateQuestion(question) {
+    try {
+        let query = `UPDATE survey_copy.question
+                     SET question = $1,
+                         type_id = $2,
+                         category_id = $3,
+                         lecture_id = $4
+                     WHERE id = $5`;
+        await dbConfig.pool.query(query,
+            [question.text,
+                parseInt(question.type_id),
+                parseInt(question.category_id),
+                parseInt(question.lecture_id),
+                parseInt(question.id)]);
+    } catch (e) {
+        console.error(e);
+        throw e;
+    }
+}
+
+
 async function getQuestionCategories() {
     try {
         let result = await dbConfig.pool.query("SELECT *  FROM survey_copy.question_category");
         console.info(new Date() + ": Getting question categories success");
+        return result;
+    } catch (e) {
+        console.error(e);
+        throw e;
+    }
+}
+
+async function getQuestionsByLectureId(lecture_id) {
+    try {
+        let sql = `SELECT q.*, t.name AS type, c.name AS category
+                   FROM survey_copy.question q
+                   JOIN survey_copy.question_category c
+                   ON q.category_id = c.id
+                   JOIN survey_copy.question_type t
+                   ON q.type_id = t.id
+                   WHERE lecture_id = $1;
+                  `;
+        let result = await dbConfig.pool.query(sql, [lecture_id]);
+        console.info(new Date() + ": Getting questions by lecture id success");
         return result;
     } catch (e) {
         console.error(e);
@@ -141,8 +190,32 @@ async function postQuestionOptions(id, options) {
         console.error(e);
         throw e;
     }
-
 }
+
+async function getSubjectsByProfessorId(id) {
+    try {
+        let result = await dbConfig.pool.query("SELECT * from survey_copy.subject " +
+            "WHERE professor_id = $1;", [id]);
+        console.info(new Date() + ": Getting professor's subjects success");
+        return result;
+    } catch (e) {
+        console.error(e);
+        throw e;
+    }
+}
+
+async function getQuestion(id) {
+    try {
+        let result = await dbConfig.pool.query(
+            "SELECT * from survey_copy.question WHERE id = $1;", [id]);
+        console.info(new Date() + ": Getting question success");
+        return result;
+    } catch (e) {
+        console.error(e);
+        throw e;
+    }
+}
+
 
 module.exports = {
     getSubjects,
@@ -151,10 +224,15 @@ module.exports = {
     getQuestions,
     getQuestionCategories,
     getQuestionTypes,
+    getQuestionsByLectureId,
     getSurveysWithSubjectNames,
     getLecturesWithSubjectNames,
+    getSubjectsByProfessorId,
     deleteSurvey,
+    deleteQuestion,
     postQuestion,
+    getQuestion,
     postQuestionOptions,
-    postLecture
+    postLecture,
+    updateQuestion
 };
